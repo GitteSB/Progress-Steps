6b33b058517bd5f337110a9084bf3d38357d7634
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2021-04-10 11:19:29 +0200

    Changes to script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-const progress = document.getElementById
+const progress = document.getElementById('progress')
+const prev= document.getElementById('prev')
+const next = document.getElementById('next')
+const progress = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click',() => {
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+
+    }
+})
+
+prev.addEventListener('click', () =>{
+    currentActive--
+
+    if(currentActive< 1){
+        currentActive = 1
+    }
+    update ()
+})
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive.add('active')) {
+            circle.classList.add('acitve')
+        }   
+        else (
+            circle.classList.remove('active')
+        )
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length -1) / (circles.length -1) * 100 +'%'
+
+    if(currentActive ===1){
+        prev.disabled = true
+    }else if (currentActive === circles.length)
+    {
+        next.disabled = true}
+        else {
+            prev.disabled = false
+            next.disabled = false
+        }
+    }
+
